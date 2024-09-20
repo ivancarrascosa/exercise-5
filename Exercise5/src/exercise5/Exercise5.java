cad83ac65608c89db4f0344edb63abcc14b93808
ivan.carrascosa <ivan.carrascosa@106-06.info2>
2024-09-20 14:04:05 +0200

    Ejercicio 5 terminado

---
+++ Exercise5/src/exercise5/Exercise5.java
@@ -0,0 +1,12 @@
+package exercise5;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("Iván Carrascosa Barragán");
+		System.out.println("Calle Flor de Adelfa");
+		System.out.println("717707565");
+
+	}
+
+}
